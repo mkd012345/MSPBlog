00410d515a5c41851d5de62c152779ace470d23c
Manthan Dubey <dubeymanthan728@gmail.com>
2025-03-19 23:13:46 +0530

    Home Login Register And Login Add
    Add Components in Home Login and Register Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,29 @@
-import BlogPage from "./BlogPage";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
-function App() {
-  return <BlogPage />;
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="p-8">
+        {/* Navigation Links */}
+        <nav className="mb-4">
+          <Link to="/" className="mr-4 text-blue-500 hover:underline">Home</Link>
+          <Link to="/login" className="mr-4 text-blue-500 hover:underline">Login</Link>
+          <Link to="/register" className="text-blue-500 hover:underline">Register</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
