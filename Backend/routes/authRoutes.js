d26a3456092af8029549a5047ca85e0ed06ea0f3
Manthan Dubey <dubeymanthan728@gmail.com>
2025-04-17 00:52:02 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/userController");
-const { getProfile } = require("../controllers/userController");
-const authenticateToken = require("../middlewares/authMiddleware"); // Importing the middleware
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
